93f6f57a9e2bda0ba7da0cdf88947639964c1e7a
tengwenxin0302 <1211306611@student.mmu.edu.my>
2023-07-13 14:13:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xFF, 0xAA, 0.2);
+		const ambient = new THREE.HemisphereLight(0xFFF, 0xAA, 0.2);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
